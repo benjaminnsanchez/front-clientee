fbbce3b2c7d06575baccf0e6dc8f95e3858401f7
benjaminn <buenardopolis10@gmail.com>
2025-06-10 01:50:13 -0300

    intento de arreglar el login 2.0

--- src/componentes/login.jsx
+++ src/componentes/login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 const [user,setUser] = useState("")
 const [password,setPassword] = useState("")
-/* contra = "1234"
+contra = "1234"
 usuario = "pepe"
 const handleLogin = (event)=>{
   event.preventDefault()
@@ -9,13 +9,13 @@ const handleLogin = (event)=>{
   if(user==usuario && password==contra){
  console.log("increíble react")
   }
-} */
+}
 const Login = ()=>{
     return <>
      <h1>loginn</h1>
      <form onSubmit={handleLogin}>
-      <input required type="text" name={user} placeholder='Ingrese tu usuario' onChange={(event)=>setUser(event.target.value)} /> <br></br>
-      <input required type="password" name={password} placeholder='Ingrese la contraseña de tu usuario' onChange={(event)=>setPassword(event.target.value)}/><br></br>
+      <input required type="text" name="user" placeholder='Ingrese tu usuario' onChange={(event)=>setUser(event.target.value)} /> <br></br>
+      <input required type="password" name="password" placeholder='Ingrese la contraseña de tu usuario' onChange={(event)=>setPassword(event.target.value)}/><br></br>
       <button></button>
      </form>
     </>
