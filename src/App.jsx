a171f77a3a82a453db6b0d1e6d145152e9f378ae
benjaminn <buenardopolis10@gmail.com>
2025-06-09 23:03:41 -0300

    agregue una la ruta home

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
       <Routes>
           <Route path="/" element={<Home />}>
             <Route path="login" element={<Login />} />
+            <Route path="/" element={<Home />} />
           </Route>
       </Routes>
     </div>
--- src/componentes/home.jsx
+++ src/componentes/home.jsx
@@ -2,6 +2,7 @@ import { Outlet , Link} from "react-router-dom"
 const Home = () =>{
     return <div>
     <Link to={"/login"}>Login</Link>
+    <Link to={"/"}>Home</Link>
     <Outlet />
     </div>
 }
