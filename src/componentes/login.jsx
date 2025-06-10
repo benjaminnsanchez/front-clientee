8def1635ccc364787638cdfc72f1640c8910489a
benjaminn <buenardopolis10@gmail.com>
2025-06-09 23:38:27 -0300

    agregue un login que todavia no sirve

--- src/componentes/login.jsx
+++ src/componentes/login.jsx
@@ -4,20 +4,20 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 return (
-    <>
-     <h1></h1>
-     <form action="">
-      <input required type="text" placeholder='Ingrese tu usuario' />
-      <input required type="password"  placeholder='Ingrese la contraseña de tu usuario'/>
-      <button type="submit"></button>
-     </form>
-    </>
+    
   )
 }
 
 export default App
  */
 const Login = ()=>{
-    return <h1>Login</h1>
+    return <>
+     <h1></h1>
+     <form action="">
+      <input required type="text" placeholder='Ingrese tu usuario' /> <br></br>
+      <input required type="password"  placeholder='Ingrese la contraseña de tu usuario'/><br></br>
+      <button type="submit"></button>
+     </form>
+    </>
 }
 export default Login
